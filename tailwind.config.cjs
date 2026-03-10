cafaa99f62a814c872419944ac2e52875c4e3f73
kimberlyn briceño <kimberlynbriceno22@gmail.com>
2026-03-10 09:59:07 -0400

    add of hero sembol

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {fontFamily: {
+        // 'montserrat' será el nombre de la clase: font-montserrat
+        montserrat: ['"Montserrat"', 'sans-serif'], 
+       },
+  },
   },
   plugins: [],
 };
